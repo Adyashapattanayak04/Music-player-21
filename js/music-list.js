cfcb6261b33fe6e8abee8a897180de2e6e712962
adyasha pattanayak <58504519+Adyashapattanayak04@users.noreply.github.com>
2021-08-04 23:05:39 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,437 @@
+
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+
+
+  {
+    name: "Ae Dil Hai Muskhil",
+    artist: "Arijit Singh",
+    img: "Ae-dil-hai-Muskil",
+    src: "Ae-dil-hai-Muskil"
+  },
+
+  {
+    name: "Agar Tum Saath Ho",
+    artist: "Arijit Singh & Alka Yagnik",
+    img: "Agar Tum Saath Ho",
+    src: "Agar Tum Saath Ho"
+  },
+
+  
+  {
+    name: "On My Way",
+    artist: "Alan Walker,Sabrina Carpenter&Farruko ",
+    img: "On My Way",
+    src: "On My Way"
+  },
+
+
+  {
+    name: "Stuck with U",
+    artist: "Ariana Grande & Justin Bieber",
+    img: "Stuck with U",
+    src: "Stuck with U"
+  },
+  
+
+  {
+    name: "Ashe",
+    artist:" - Moral of the Story",
+    img: "Ashe - Moral of the Story",
+    src: "Ashe - Moral of the Story"
+  },
+
+
+  {
+    name: "Heaven",
+    artist:"Avicii",
+    img: "Avicii - Heaven",
+    src: "Avicii - Heaven"
+  },
+
+
+
+  {
+    name: "Show Me The Meaning Of Being Lonely",
+    artist:"Backstreet Boys",
+    img: "Backstreet Boys - Show Me The Meaning Of Being Lonely",
+    src: "Backstreet Boys - Show Me The Meaning Of Being Lonely"
+  },
+
+
+  
+  {
+    name: "I Can't Get Enough",
+    artist:"benny blanco, Tainy, Selena Gomez",
+    img: "benny blanco, Tainy, Selena Gomez, J. Balvin - I Can't Get Enough",
+    src: "benny blanco, Tainy, Selena Gomez, J. Balvin - I Can't Get Enough"
+  },
+
+  
+  {
+    name: "Bury a friend",
+    artist:"Billie Eilish",
+    img: "Billie Eilish - bury a friend",
+    src: "Billie Eilish - bury a friend"
+  },
+
+
+  
+  {
+    name: "lovely",
+    artist:"Billie Eilish, Khalid",
+    img: "Billie Eilish, Khalid - lovely",
+    src: "Billie Eilish, Khalid - lovely"
+  },
+
+
+  {
+    name: "'Butter'",
+    artist:"BTS-Bangtan Boys",
+    img: "BTS (방탄소년단) 'Butter'",
+    src: "BTS (방탄소년단) 'Butter'"
+  },
+
+  {
+    name: "'DNA'",
+    artist:"BTS-Bangtan Boys",
+    img: "BTS (방탄소년단) 'DNA' Official MV",
+    src: "BTS (방탄소년단) 'DNA' Official MV"
+  },
+
+  {
+    name: "Blood Sweat & Tears",
+    artist:"BTS-Bangtan Boys",
+    img: "BTS (방탄소년단) '피 땀 눈물 (Blood Sweat & Tears)' Official MV",
+    src: "BTS (방탄소년단) '피 땀 눈물 (Blood Sweat & Tears)' Official MV"
+  },
+
+  {
+    name: "Bulleya",
+    artist:"Pritam Chakraborty",
+    img: "Bulleya",
+    src: "Bulleya"
+  },
+
+  {
+    name: "Biblical",
+    artist:"Calum Scott",
+    img: "Calum Scott - Biblical (Official Music Video)",
+    src: "Calum Scott - Biblical (Official Music Video)"
+  },
+
+
+  {
+    name: "Havana",
+    artist:"Camila Cabello",
+    img: "Camila Cabello - Havana (Audio) ft. Young Thug",
+    src: "Camila Cabello - Havana (Audio) ft. Young Thug"
+  },
+
+
+  {
+    name: "Shameless",
+    artist:"Camila Cabello",
+    img: "Camila Cabello - Shameless",
+    src: "Camila Cabello - Shameless"
+  },
+
+
+  {
+    name: "We Don't Talk Anymore",
+    artist:"Charlie Puth (feat. Selena Gomez)",
+    img: "Charlie Puth - We Don't Talk Anymore (feat. Selena Gomez) [Official Video]",
+    src: "Charlie Puth - We Don't Talk Anymore (feat. Selena Gomez) [Official Video]"
+  },
+
+
+  {
+    name: "Con Calma",
+    artist:"Daddy Yankee &amp Snow",
+    img: "Daddy Yankee &amp Snow - Con Calma (Video Oficial)",
+    src: "Daddy Yankee &amp Snow - Con Calma (Video Oficial)"
+  },
+
+
+  
+  {
+    name: "Perfect",
+    artist:"Ed Sheeran",
+    img: "Ed Sheeran - Perfect",
+    src: "Ed Sheeran - Perfect"
+  },
+
+
+  
+  {
+    name: "Shape of You",
+    artist:"Ed Sheeran",
+    img: "Ed Sheeran - Shape of You",
+    src: "Ed Sheeran - Shape of You"
+  },
+
+
+
+  
+  {
+    name: "Thinking Out Loud",
+    artist:"Ed Sheeran",
+    img: "Ed Sheeran - Thinking Out Loud",
+    src: "Ed Sheeran - Thinking Out Loud"
+  },
+
+
+
+  
+  {
+    name: "Love Me Like You Do - Fifty Shades of Grey",
+    artist:"Ellie Goulding",
+    img: "Ellie Goulding - Love Me Like You Do - Fifty Shades of Grey",
+    src: "Ellie Goulding - Love Me Like You Do - Fifty Shades of Grey"
+  },
+
+
+
+  
+  {
+    name: "Bad Dreams",
+    artist:"Faouzia",
+    img: "Faouzia - Bad Dreams (Stripped)",
+    src: "Faouzia - Bad Dreams (Stripped)"
+  },
+
+
+  
+  {
+    name: "Falling",
+    artist:"Harry Styles",
+    img: "Harry Styles - Falling",
+    src: "Harry Styles - Falling"
+  },
+
+  {
+    name: "Mi Gente",
+    artist:"J Balvin, Willy William",
+    img: "J Balvin, Willy William - Mi Gente)",
+    src: "J Balvin, Willy William - Mi Gente)"
+  },
+
+
+
+  {
+    name: "Comethru",
+    artist:"Jeremy Zucker",
+    img: "Jeremy Zucker - comethru",
+    src: "Jeremy Zucker - comethru"
+  },
+
+
+  {
+    name: "Sucker",
+    artist:"Jonas Brothers",
+    img: "Jonas Brothers - Sucker",
+    src: "Jonas Brothers - Sucker"
+  },
+
+  {
+    name: "If the World Was Ending",
+    artist:"JP Saxe ft. Julia Michaels",
+    img: "JP Saxe - If the World Was Ending ft. Julia Michaels",
+    src: "JP Saxe - If the World Was Ending ft. Julia Michaels"
+  },
+
+
+  {
+    name: "What Do You Mean",
+    artist:"Justin Bieber",
+    img: "Justin Bieber - What Do You Mean",
+    src: "Justin Bieber - What Do You Mean"
+  },
+
+
+  {
+    name: "Can We Kiss Forever",
+    artist:"Kina ft.Adriana Proenza",
+    img: "Kina - Can We Kiss Forever (Lyrics) ft. Adriana Proenza",
+    src: "Kina - Can We Kiss Forever (Lyrics) ft. Adriana Proenza"
+  },
+
+  {
+    name: "Shallow (from A Star Is Born)",
+    artist:"Lady Gaga, Bradley Cooper",
+    img: "Lady Gaga, Bradley Cooper - Shallow (from A Star Is Born)",
+    src: "Lady Gaga, Bradley Cooper - Shallow (from A Star Is Born)"
+  },
+
+  {
+    name: "Someone You Loved",
+    artist:"Lewis Capaldi",
+    img: "Lewis Capaldi - Someone You Loved",
+    src: "Lewis Capaldi - Someone You Loved"
+  },
+
+
+  {
+    name: "The Night We Met",
+    artist:"Lord Huron",
+    img: "Lord Huron - The Night We Met",
+    src: "Lord Huron - The Night We Met"
+  },
+
+  {
+    name: "Girls Like You",
+    artist:"Maroon 5 ft. Cardi B",
+    img: "Maroon 5 - Girls Like You ft. Cardi B (Official Music Video)",
+    src: "Maroon 5 - Girls Like You ft. Cardi B (Official Music Video)"
+  },
+  {
+    name: "death bed(coffee for your head)",
+    artist:"Powfu ft. beabadoobee",
+    img: "Powfu - death bed (coffee for your head)ft. beabadoobee",
+    src: "Powfu - death bed (coffee for your head)ft. beabadoobee"
+  },
+
+  {
+    name: "Raabta",
+    artist:"Arjit Singh",
+    img: "raabta-arjit-singh",
+    src: "raabta-arjit-singh"
+  },
+
+  {
+    name: "Lost Boy",
+    artist:"Ruth B.",
+    img: "Ruth B. - Lost Boy",
+    src: "Ruth B. - Lost Boy"
+  },
+
+  {
+    name: "Lose You To Love Me",
+    artist:"Selena Gomez",
+    img: "Selena Gomez - Lose You To Love Me",
+    src: "Selena Gomez - Lose You To Love Me"
+  },
+
+  {
+    name: "Stitches",
+    artist:"Shawn Mendes",
+    img: "Shawn Mendes - Stitches",
+    src: "Shawn Mendes - Stitches"
+  },
+
+  {
+    name: "Treat You Better",
+    artist:"Shawn Mendes",
+    img: "Shawn Mendes - Treat You Better",
+    src: "Shawn Mendes - Treat You Better"
+  },
+
+  {
+    name: "Señorita",
+    artist:"Shawn Mendes, Camila Cabello",
+    img: "Shawn Mendes, Camila Cabello - Señorita",
+    src: "Shawn Mendes, Camila Cabello - Señorita"
+  },
+
+  {
+    name: "You broke me first",
+    artist:"Tate McRae",
+    img: "Tate McRae - you broke me first (Official Video)",
+    src: "Tate McRae - you broke me first (Official Video)"
+  },
+
+  {
+    name: "Teardrops On My Guitar",
+    artist:"Taylor Swift",
+    img: "Teardrops On My Guitar - Taylor Swift (Lyrics) 🎵",
+    src: "Teardrops On My Guitar - Taylor Swift (Lyrics) 🎵"
+  },
+
+  {
+    name: "Closer",
+    artist:"The Chainsmokersft. Halsey",
+    img: "The Chainsmokers - Closer (Lyric) ft. Halsey",
+    src: "The Chainsmokers - Closer (Lyric) ft. Halsey"
+  },
+
+  {
+    name: "After Hours",
+    artist:"The Weeknd",
+    img: "The Weeknd - After Hours (Audio)",
+    src: "The Weeknd - After Hours (Audio)"
+  },
+
+  {
+    name: "Past Life",
+    artist:"Trevor Daniel,Selena Gomez",
+    img: "Trevor Daniel, Selena Gomez - Past Life (Official Video)",
+    src: "Trevor Daniel, Selena Gomez - Past Life (Official Video)"
+  },
+
+  {
+    name: "My Heart Will Go On",
+    artist:"Titanic",
+    img: "Titanic - My Heart Will Go On",
+    src: "Titanic - My Heart Will Go On"
+  },
+
+  {
+    name: "See You Again ",
+    artist:"Wiz Khalifa ft. Charlie",
+    img: "Wiz Khalifa - See You Again ft. Charlie",
+    src: "Wiz Khalifa - See You Again ft. Charlie"
+  },
+
+  {
+    name: "Dusk Till Dawn",
+    artist:"ZAYN ft. Sia",
+    img: "ZAYN - Dusk Till Dawn ft. Sia",
+    src: "ZAYN - Dusk Till Dawn ft. Sia"
+  },
+
+
+];
